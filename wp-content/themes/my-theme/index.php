0229be22fa210e9d521edf280829d73f8d8f33c5
EleonoraSaba <37735707+EleonoraSaba@users.noreply.github.com>
2018-07-02 13:50:58 +0200

    Update index.php

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -14,10 +14,10 @@ get_header();?>
       <img class="d-block w-100 img-fluid height: 500px" src="https://images.musement.com/cover/0001/09/italia-cagliari-veduta-mare_header-8090.jpeg?&q=60&fit=crop" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="http://www.settenanielmas.it/wp-content/uploads/2017/06/Cagliari-02.jpg" alt="Second slide">
+      <img class="d-block w-100  img-fluid height: 500px" src="http://www.settenanielmas.it/wp-content/uploads/2017/06/Cagliari-02.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="http://www.saily.it/sites/default/files/_poetto.jpg" alt="Third slide">
+      <img class="d-block w-100  img-fluid height: 500px" src="http://www.saily.it/sites/default/files/_poetto.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
