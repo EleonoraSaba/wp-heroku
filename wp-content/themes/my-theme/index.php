0514bd9030972b1bb723c40a9d36efc647ca1054
EleonoraSaba <37735707+EleonoraSaba@users.noreply.github.com>
2018-07-03 12:31:28 +0200

    Update index.php

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -11,7 +11,7 @@ get_header();?>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100 " style="height: 500px " src="img/poetto.jpg" alt="First slide">
+      <img class="d-block w-100 " style="height: 500px " src="https://www.unica.it/static/resources/cms/images/008808b4fc8acf679e5910f46b62806a_d0.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
       <img class="d-block w-100"  style="height: 500px " src="http://www.settenanielmas.it/wp-content/uploads/2017/06/Cagliari-02.jpg" alt="Second slide">
@@ -44,11 +44,11 @@ get_header();?>
             irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="col-md-5 align-self-center">
-          <img class="img d-block w-100 img-thumbnail" src="Bastione.PNG"> </div>
+          <img class="img d-block w-100 img-thumbnail" src="http://sardegna.admaioramedia.it/wp-content/uploads/bastione.jpg"> </div>
       </div>
       <div class="row">
         <div class="col-md-5">
-          <img class="img-fluid d-block mb-4 w-100 img-thumbnail" src="https://pingendo.github.io/templates/sections/assets/gallery_3.jpg"> </div>
+          <img class="img-fluid d-block mb-4 w-100 img-thumbnail" src="https://upload.wikimedia.org/wikipedia/it/thumb/b/b8/CagliariStemma2015.png/300px-CagliariStemma2015.png"> </div>
         <div class="col-md-7">
           <h2 class="text-primary pt-3" id="chisiamo">Article subtitle #2</h2>
           <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
