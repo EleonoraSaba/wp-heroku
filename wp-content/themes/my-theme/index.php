6c14037d6c11ace54e7537be0d4d13117713ef62
EleonoraSaba <37735707+EleonoraSaba@users.noreply.github.com>
2018-07-03 11:40:57 +0200

    Update index.php

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -39,7 +39,7 @@ get_header();?>
     <div class="container">
       <div class="row mb-5">
         <div class="col-md-7">
-          <h2 class="text-primary">Article subtitle #1</h2>
+          <h2 class="text-primary" id="blog">Article subtitle #1</h2>
           <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
             irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
@@ -50,7 +50,7 @@ get_header();?>
         <div class="col-md-5">
           <img class="img-fluid d-block mb-4 w-100 img-thumbnail" src="https://pingendo.github.io/templates/sections/assets/gallery_3.jpg"> </div>
         <div class="col-md-7">
-          <h2 class="text-primary pt-3">Article subtitle #2</h2>
+          <h2 class="text-primary pt-3" id="chisiamo">Article subtitle #2</h2>
           <p class="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
             irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
